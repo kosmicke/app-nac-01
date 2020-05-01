fc197ae3fb8f82986f0c291c5a83c23a0520b8e5
André Rodrigues <andre.urbanico@gmail.com>
2020-05-01 14:24:03 -0300

    exemplo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExampleComponent } from './example/example/example.component';
 
 const routes: Routes = [
+  // APLIQUE SUAS CONFIGURAÇÕES DE ROTAS AQUI
+  // {...}
+
+  // ESTAS SÃO ROTAS DE EXEMPLO
+  {
+    path: "example",
+    component : ExampleComponent
+  },
   {
     path: "",
-    children : []
+    redirectTo : "/example",
+    pathMatch : "full"
+  },
+  {
+    path: "**",
+    component : ExampleComponent
   }
 ];
 
---
+++ src/app/example/example.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExampleComponent } from './example/example.component';
+
+
+
+@NgModule({
+  declarations: [ExampleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExampleModule { }
